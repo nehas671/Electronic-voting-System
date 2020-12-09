b9ef4a5238967ec17db01ed801fba336012890af
Damini <daminikumari21@gmail.com>
2020-12-09 13:20:20 +0530

    comitting candidate

--- ElectronicVotingSystem/src/test/java/com/cg/votingapp/presentation/CandidateControllerTest.java
+++ ElectronicVotingSystem/src/test/java/com/cg/votingapp/presentation/CandidateControllerTest.java
@@ -4,9 +4,6 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
-import java.util.HashSet;
-import java.util.Set;
-
 import org.junit.BeforeClass;
 import org.junit.Test;
 
