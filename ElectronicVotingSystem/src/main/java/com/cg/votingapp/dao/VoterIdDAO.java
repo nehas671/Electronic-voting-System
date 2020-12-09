f47a30130f4ec8f935e911d5612ea406fdab1443
Soumyashri <soumyashrikulkarni31@gmail.com>
2020-12-09 09:48:22 +0530

    commit by soumyashri

--- ElectronicVotingSystem/src/main/java/com/cg/votingapp/dao/VoterIdDAO.java
+++ ElectronicVotingSystem/src/main/java/com/cg/votingapp/dao/VoterIdDAO.java
@@ -1,11 +1,12 @@
 package com.cg.votingapp.dao;
 
+import com.cg.votingapp.exceptions.ReqNotFoundException;
 import com.cg.votingapp.entity.VoterIdEntity;
 
 import com.cg.votingapp.exceptions.UserNotFoundException;
 
 public interface VoterIdDAO {
 	VoterIdEntity findByUserId(int user_id) throws UserNotFoundException;
-
+	VoterIdEntity findByReqId(int request_id) throws ReqNotFoundException;
 }
 
