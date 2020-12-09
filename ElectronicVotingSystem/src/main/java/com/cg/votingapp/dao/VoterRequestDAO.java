e6877af8d5d42456ac1cf9341ee050e20387b615
jyoti <jyotitiwari631@gmail.com>
2020-12-09 16:38:58 +0530

    commit

--- ElectronicVotingSystem/src/main/java/com/cg/votingapp/dao/VoterRequestDAO.java
+++ ElectronicVotingSystem/src/main/java/com/cg/votingapp/dao/VoterRequestDAO.java
@@ -1,5 +1,15 @@
 package com.cg.votingapp.dao;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+import javax.persistence.TypedQuery;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+
 public interface VoterRequestDAO {
 
 	VoterRequestEntity addVoterRequest(VoterRequestEntity entity);
---
+++ ElectronicVotingSystem/src/main/java/com/cg/votingapp/presentation/CandidateController2.java
@@ -0,0 +1,63 @@
+package com.cg.votingapp.presentation;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.cg.votingapp.dto.Candidate;
+import com.cg.votingapp.entity.CandidateEntity;
+import com.cg.votingapp.exceptions.NullValueFoundException;
+import com.cg.votingapp.exceptions.RecordNotFoundException;
+import com.cg.votingapp.service.CandidateService;
+import com.cg.votingapp.service.CandidateServiceImpl;
+
+public class CandidateController {
+	private static Logger logger = LogManager.getLogger(CandidateController.class.getName());
+	CandidateService candidateService = new CandidateServiceImpl();
+	
+	public Candidate addCandidate(Candidate candidate)
+	{
+		logger.info("Adding Candidate");
+		Candidate c=candidateService.addCandidate(candidate);
+	    return c;
+	}
+	
+	public Candidate checkId(int candidateId) throws NullValueFoundException {
+		logger.info("Checking candidate for id: " + candidateId);
+		Candidate candidate = null;
+		try {
+			candidate = candidateService.checkId(candidateId);
+		}
+		catch(NullValueFoundException e) {
+			logger.error("NullValueFoundException: " + e);
+			throw new NullValueFoundException(e.getMessage());
+		}
+		return candidate;
+	}
+	
+	public Candidate checkName(String candidateName) throws NullValueFoundException {
+		logger.info("Checking candidate for name: " + candidateName);
+		Candidate candidate = null;
+		try {
+			candidate = candidateService.checkName(candidateName);
+		}
+		catch(NullValueFoundException e) {
+			logger.error("NullValueFoundException: " + e);
+			throw new NullValueFoundException(e.getMessage());
+		}
+		return candidate;
+	}
+	
+	public Candidate viewCandidate(int candidate_id) throws RecordNotFoundException{
+		logger.info("Viewing Candidate List");
+		Candidate candidate= null;
+		try {
+			candidate = candidateService.viewCandidate(candidate_id);
+		}
+		catch(Exception e) {
+			logger.error("RecordNotFoundException: " + e);
+			throw new RecordNotFoundException(e.getMessage());
+		}
+		return candidate;
+    }
+
+}
