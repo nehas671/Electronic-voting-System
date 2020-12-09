78d52d507e02a43d09512fff499f241d14db84de
jyoti <jyotitiwari631@gmail.com>
2020-12-09 17:27:20 +0530

    com

--- ElectronicVotingSystem/src/main/java/com/cg/votingapp/service/VoterRequestService.java
+++ ElectronicVotingSystem/src/main/java/com/cg/votingapp/service/VoterRequestService.java
@@ -3,8 +3,9 @@ package com.cg.votingapp.dao;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
-import javax.persistence.Query;
-import javax.persistence.TypedQuery;
+import com.cg.votingapp.exceptions.NullValueFoundException;
+import com.cg.votingapp.exceptions.RecordNotFoundException;
+import com.cg.votingapp.entity.VoterRequest;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -13,7 +14,7 @@ import org.apache.logging.log4j.Logger;
 public interface VoterRequestService {
 
 	VoterRequest addVoterRequest(VoterRequest voterRequest );
-	VoterRequest checkId(int userId) throws NullValueFoundException;
-	VoterRequest checkName(String userName) throws NullValueFoundException;
+	VoterRequest checkById(int userId) throws NullValueFoundException;
+	VoterRequest checkByName(String userName) throws NullValueFoundException;
 	VoterRequest viewVoterRequest(int user_id) throws RecordNotFoundException;;
 }
