51169cce6bd77f8d79a98f9fb9becdf928a13710
Aachal <aachaldaharwal@gmail.com>
2020-12-09 17:19:45 +0530

    commiting

--- ElectronicVotingSystem/src/test/java/com/cg/votingapp/presentation/UpdateCandidateControllerTest.java
+++ ElectronicVotingSystem/src/test/java/com/cg/votingapp/presentation/UpdateCandidateControllerTest.java
@@ -1,6 +1,19 @@
 package com.cg.votingapp.presentation;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.BeforeClass;
+
 public class UpdateCandidateControllerTest {
 
+	private static UpdateCandidateController updatecandidateController;
+	private static Logger logger;
+	
+	@BeforeClass
+	public static void setUp() {
+		logger = LogManager.getLogger(UpdateCandidateControllerTest.class.getName());
+		updatecandidateController = new UpdateCandidateController();
+	}
+	
 	
 }
