4d0ddfac73b65c076e5ed44e5c723bbff187fe13
Aachal <aachaldaharwal@gmail.com>
2020-12-09 12:32:36 +0530

    committing

--- ElectronicVotingSystem/src/main/java/com/cg/votingapp/dao/CandidateDAO.java
+++ ElectronicVotingSystem/src/main/java/com/cg/votingapp/dao/CandidateDAO.java
@@ -2,11 +2,17 @@ package com.cg.votingapp.dao;
 
 import com.cg.votingapp.entity.CandidateEntity;
 import com.cg.votingapp.exceptions.NullValueFoundException;
-import com.cg.votingapp.exceptions.RecordNotFoundException;
 
 public interface CandidateDAO {
 	public CandidateEntity addCandidate(CandidateEntity entity);
 	CandidateEntity checkId(int candidateId) throws NullValueFoundException;
 	CandidateEntity checkName(String candidateName) throws NullValueFoundException;
-	public CandidateEntity viewCandidate(int candidate_id) throws RecordNotFoundException;
+	boolean viewCandidate();
+	//public List<CandidateEntity> viewCandidate();
+	public CandidateEntity updateCandidateName(String candidate_id, String candidate_name) throws NullValueFoundException ;
+	public CandidateEntity updateAddress(String candidate_id, String address) throws NullValueFoundException ;
+	public CandidateEntity updateAge(String candidate_id, int age) throws NullValueFoundException ;
+	public CandidateEntity updateContactDetails(String candidate_id, int contact_details) ;
+
+
 }
